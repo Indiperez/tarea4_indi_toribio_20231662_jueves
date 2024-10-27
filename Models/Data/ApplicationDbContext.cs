798f1b477ba1674229ecfb96beb6b91f81d765d8
gianrobert ramirez <gianrobert13@gmail.com>
2024-10-26 21:47:08 -0400

    Agregar archivos de proyecto.

---
+++ Models/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ClientesAPI.Models;
+
+namespace ClientesAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
